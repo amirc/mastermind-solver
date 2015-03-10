f1ba01bf572298b66a88d73379328c57fd1efbe1
Amir Lellouche <lalosher@gmail.com>
2015-03-10 11:42:00 +0200

    coding style

--- Mastermind.py
+++ Mastermind.py
@@ -4,14 +4,14 @@ from collections import Counter
 
 class Game:
     def __init__(self, slots, options):
-        self.slots = slots
-        self.options = options
+        self._slots = slots
+        self._options = options
         self.guesses = set()
-        self.numGuess = 0
-        self.code = []
+        self._num_guess = 0
+        self._code = []
         for i in range(slots):
-            self.code.append(random.randint(0, options - 1))
-        self.codeCount = Counter(self.code)
+            self._code.append(random.randint(0, options - 1))
+        self._code_count = Counter(self._code)
 
     def check_guess(self, guess):
         win_status = 0
@@ -19,20 +19,20 @@ class Game:
 
         correct_digits = 0
         for digit in digit_count:
-            if digit_count[digit] > self.codeCount[digit]:
-                correct_digits += self.codeCount[digit]
+            if digit_count[digit] > self._code_count[digit]:
+                correct_digits += self._code_count[digit]
             else:
                 correct_digits += digit_count[digit]
 
         correct_slots = 0
-        for i in range(self.slots):
-            if self.code[i] == guess[i]:
+        for i in range(self._slots):
+            if self._code[i] == guess[i]:
                 correct_slots += 1
 
         self.guesses.add((tuple(guess), correct_slots, correct_digits - correct_slots))
-        self.numGuess += 1
+        self._num_guess += 1
 
-        if correct_slots == self.slots:
+        if correct_slots == self._slots:
             win_status = 1
 
         return win_status
